b0178889e0ce99003c13929ac27987a233406edb
Spusik228 <vatkin502@gmail.com>
2024-10-12 22:14:42 +0300

    Готово

--- main.cpp
+++ main.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <fstream>
 using namespace std;
-
-int main(){
-    ofstream fout("main.py");
-    fout << "print('Use C++ instead :pray')" << endl;
-    fout.close();
-
-    ofstream fout2("main.py", ios::app);
-    fout2 << "print('or at least Java')" << endl;
-    fout2.close();
-
-    
-    ifstream fin("main.cpp");
-    // ifstream fin("/home/merilian/cpp-university/09.18.2024/main.cpp");
-    // ifstream fin("../09.18.2024/main.cpp");
-    string line;
-    while(getline(fin, line)){
-        cout << line << endl;
+bool letters(string& str){
+    for (char ch : str) {
+        if (!isalpha(ch)){
+            return false;
+        }
     }
-    fin.close();
-
-    ifstream fin2("out");
-    int a, b;
-    double c;
-    fin2 >> a >> b >> c;
-    cout << a << "\t" << b << "\t" << c << "\n";
-    fin2.close(); 
+    return true;
+}
+bool digits(string& str){
+    for (char ch : str){
+        if (!isdigit(ch)){
+            return false;
+        }
+    }
+    return true;
+}
+void raspred (istream &in, fstream &out1, fstream &out2){
+    string stroka;
+    while(getline(in, stroka)){
+        if (letters(stroka)){
+            out2 << stroka << endl;
+        }
+        if (digits(stroka)){
+            out1 << stroka << endl;
+        }
+    }
+}
+void pr(istream &file){
+    string stroka;
+    file.seekg(0, ios::beg);
+    while(getline(file, stroka)){
+        cout << stroka << endl;
+    }
+}
+int main(){
+    auto &in_fname = "files/INPUT.txt";
+    ifstream in;
+    fstream out1("files/out1.txt", ios::trunc | ios_base::in | ios_base::out);
+    fstream out2("files/out2.txt", ios::trunc | ios_base::in | ios_base::out);
+    int b;
+    cout << "Введите 1 если хотите увидеть входной файл, 0 если нет " << endl;
+    cin >> b;
+    if (b == 1){
+        in.open(in_fname);
+        pr(in);
+        in.close();
+    }
+    in.open(in_fname);
+    raspred (in, out1, out2);
+    in.close();
+    cout << "Введите 0 если хотите увидеть выходной файл со словами, 1 если с цифрами и 3 если не хотите видеть, 4 если оба ";
+    cin >> b;
+    if (b == 0){
+        pr(out2);
+    }if (b == 1){
+        pr(out1);
+    }if (b == 4){
+        pr(out2);
+        pr(out1);
+    }
+    out1.close();
+    out2.close();
 }
